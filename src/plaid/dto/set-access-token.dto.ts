8886cca65b70e2b8f969478aee8ee10c4c9c5d1b
hoanthui <hoanthui123@gmail.com>
2023-09-29 16:35:30 +0700

    chore: remove unused fn

--- src/plaid/dto/set-access-token.dto.ts
+++ src/plaid/dto/set-access-token.dto.ts
@@ -1,6 +1,35 @@
-import { IsString } from 'class-validator';
+import { IsArray, IsOptional, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+export class PlaidAccount {
+  @IsString()
+  id: string;
 
+  @IsString()
+  name: string;
+
+  @IsString()
+  mask: string;
+
+  @IsString()
+  type: string;
+
+  @IsString()
+  subtype: string;
+
+  @IsString()
+  @IsOptional()
+  verification_status?: string;
+
+  @IsString()
+  @IsOptional()
+  class_type?: string;
+}
 export class SetAccessTokenDto {
   @IsString()
   readonly public_token: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => PlaidAccount)
+  readonly accounts: PlaidAccount[];
 }
